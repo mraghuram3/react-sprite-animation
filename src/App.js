c4f127978f02d18bcd5a75a085b96c5779a01bc9
Raghu <raghu@vimals-MacBook-Pro-2.local>
2022-10-12 00:11:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,12 @@ function App() {
         <br />
         <br />
         <Sprite  spriteImg={require('./idle.png')} frames={10}/>
+        <Sprite
+        spriteImg={
+          'https://miro.medium.com/max/1400/1*GJ6ZL0eX8tJE54ZYFSnI9Q.png'
+        }
+        frames={8}
+      />
     </div>
   );
 }
