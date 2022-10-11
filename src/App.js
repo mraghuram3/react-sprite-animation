08c9af78cf0b39594434771b349d675d0ac8cfac
Raghu <raghu@vimals-MacBook-Pro-2.local>
2022-10-11 23:22:11 +0530

    added initial sprite component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+
+import { Sprite } from './lib';
 
 import './App.css';
 
@@ -5,6 +8,9 @@ function App() {
   return (
     <div className="App">
         react-sprite-animation
+        <br />
+        <br />
+        <Sprite />
     </div>
   );
 }
---
+++ src/lib/sprite/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './style.module.css';
+
+export function Sprite() {
+  return (
+    <div className={styles.container}>
+        Sprite Container
+    </div>
+  );
+}
